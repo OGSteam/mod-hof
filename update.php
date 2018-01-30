d42f1b152c72eb79d0e648f41e38c9181f4a4156
Anthony <darknoon@darkcity.fr>
2018-01-30 19:52:11 +0100

    Ajout Modification Table + Version

--- update.php
+++ update.php
@@ -7,7 +7,14 @@ if (!defined('IN_SPYOGAME'))
 
 $mod_folder = 'hof';
 $mod_name = 'hof';
+define('TABLE_HOF_PROD', $table_prefix . 'hof_prod');
+
+//Update Table
+global $db;
+
+$query = "ALTER TABLE `" . TABLE_HOF_PROD . "` ADD `t` DECIMAL(15,6) UNSIGNED NOT NULL AFTER `d`";
+
+$db->sql_query($query);
 
 update_mod($mod_folder, $mod_name);
 
-?>
